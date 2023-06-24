8f6d1fd759a5a94faaf5108795c71ae442ab4383
Javi <javiermoyasimo@gmail.com>
2023-06-24 18:10:06 +0200

    feat:Cors policy added

--- src/init.js
+++ src/init.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const router = require("./routers");
 const PORT = process.env.PORT || 3001;
+//Cors options
+const cors = require('cors')
+var corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
+// Middlewares
 app.use(express.json());
+app.use(cors(corsOptions));
+
+//Rutas
 app.use(router);
 
 app.listen(PORT, () => {
