53712915b2dfeda0971cb2a51f3eb98ddc32423c
Javi <javiermoyasimo@gmail.com>
2023-06-24 18:24:58 +0200

    feat: Create user-model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,1 +1,30 @@
 // TODO ESTO ES NUEVO!
+const mongoose = require("../db");
+
+const userSchema = {
+  name: {
+    type: "String",
+    required: true,
+    minLength: 2,
+    maxLength: 25,
+  },
+  phone: {
+    type: "String",
+    required: true,
+    minLength: 9,
+    maxLength: 9,
+  },
+  email: {
+    type: "String",
+    unique: true,
+  },
+  password: "String",
+  create: {
+    type: Date,
+    default: Date.now,
+  },
+};
+
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
